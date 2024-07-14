188d422e5d9798f11453bd1751c0cc7df7a0d6c9
imabuzerkhan <abuzerk41@gmail.com>
2024-07-14 17:24:18 +0545

    header complete

---
+++ src/components/layout/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+   <>
+   <div className="mai-div">
+    <div className="content"></div>
+    <div className="image"></div>
+   </div>
+   </>
+  )
+}
+
+export default Hero
