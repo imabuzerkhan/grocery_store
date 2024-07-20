ca39ffeed6662d81c3eae3a214dfca684b32684c
imabuzerkhan <abuzerk41@gmail.com>
2024-07-20 07:46:22 +0545

    new commit

---
+++ src/_service/GlobalApi.js
@@ -0,0 +1,27 @@
+const Master_url = 'https://ap-south-1.cdn.hygraph.com/content/clys4td3k023a08uyk4fudlyg/master'
+
+const getCategory=async()=>{
+  const query=gql`
+  categories {
+    id
+    name
+    bgColor {
+      hex
+      css
+    }
+    categoryimage {
+      url
+    }
+  }
+  `
+
+
+const result = await request(Master_url, query)
+return result
+
+
+}
+
+export default {
+  getCategory
+}
