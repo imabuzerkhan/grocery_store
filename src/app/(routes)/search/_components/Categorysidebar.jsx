3c73765412d3cfc2bcd611177754bc007fd7076b
imabuzerkhan <abuzerk41@gmail.com>
2024-07-23 21:29:44 +0545

    new commit

--- src/app/(routes)/search/_components/Categorysidebar.jsx
+++ src/app/(routes)/search/_components/Categorysidebar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import Image from "next/image";
 import GlobalApi from "@/_service/GlobalApi";
 import { useEffect, useState } from "react";
 const Categorysidebar = () => {
@@ -28,14 +29,14 @@ const Categorysidebar = () => {
       <h2 className='font-bold mb-3 text-lg  ' >Categories</h2>
       {
         categorylist.map((item , index)=>(
-<div key={index} className='flex gap-2 rounded-lg border mb-3' >
+<div key={index} className='flex items-center px-2 py-2 gap-2  md:gap-10 rounded-lg border mb-3' >
 <Image
-            src={category.categoryimage.url}
+            src={item.categoryimage.url}
             alt='icon'
-            width={80}
-            height={35}
+            width={30}
+            height={30}
           />
-          <h2>{category.name}</h2>
+          <h2>{item.name}</h2>
 
 
 </div>
