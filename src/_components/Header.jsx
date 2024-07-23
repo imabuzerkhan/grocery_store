4c2332893b667a3202d1aecb6a3445f0b35b18bc
imabuzerkhan <abuzerk41@gmail.com>
2024-07-23 23:16:41 +0545

    updated home

--- src/_components/Header.jsx
+++ src/_components/Header.jsx
@@ -5,6 +5,7 @@ import React, { useEffect } from 'react'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import { signIn, useSession } from 'next-auth/react'
+import Link from 'next/link'
 const Header = () => {
 
 const {data} = useSession();
@@ -16,13 +17,14 @@ console.log(data)
   return (
     <div className='p-5 shadow-sm flex justify-between ' >
       <div className="flex items-center gap-8 " >
-      <Image
+    <Link href={'/'} > <Image
       src={'logo.svg'}
       alt='logo'
       width={180}
       height={100}
       
       />
+      </Link> 
       <div className="text md:flex items-center gap-6 hidden ">
         <h2 className='hover:scale-105 hover:text-purple-400 cursor-pointer ' >Home</h2>
         <h2 className='hover:scale-105 hover:text-purple-400 cursor-pointer ' >Service</h2>
