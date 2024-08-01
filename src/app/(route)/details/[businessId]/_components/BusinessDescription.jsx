fe6064c2f840a54a7858da12e867adae82de2dd3
imabuzerkhan <abuzerk41@gmail.com>
2024-08-01 18:21:49 +0545

    booking section

--- src/app/(route)/details/[businessId]/_components/BusinessDescription.jsx
+++ src/app/(route)/details/[businessId]/_components/BusinessDescription.jsx
@@ -2,23 +2,34 @@ import Image from 'next/image'
 import React from 'react'
 
 function BusinessDescription({business}) {
-  return business?.name&&(
+  return  (
     <div>
-      <h2 className='font-bold text-[25px] '>Description</h2>
-      <p className='mt-4 text-lg text-gray-600'>{business.about}</p>
-
-      <h2 className='font-bold text-[25px] mt-8'>Gallary</h2>
-      <div className='grid grid-cols-2 md:grid-cols-3
+   
+      
+        {business.map((item,index)=>(
+          <>
+          <div>
+          <h2 className='font-bold text-[25px] '>Description</h2>
+          <p className='mt-4 text-lg text-gray-600'>{item.about}</p>
+          </div>
+         <div>
+         <h2 className='font-bold text-[25px] mt-8'>Gallery</h2>
+         <div className='grid grid-cols-2 md:grid-cols-3
       lg:grid-cols-4 gap-5 mt-5'>
-        {business?.images?.map((item,index)=>(
-          <Image src={item?.url} key={index}
+         <Image  src={item?.image[0]?.url || '/path/to/default/image.jpg'} key={index}
           alt='image'
           width={700}
           height={200}
           className='rounded-lg' />
-        ))}
+          
       </div>
-     </div>
+         </div>
+         </>
+         
+        ))}
+        
+      </div>
+    
   )
 }
 
